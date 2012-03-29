1a0d4e15d0d01aaecd778eda4cf477c1409c84d8
Christopher Harrison <c@xoph.co>
2012-03-29 12:13:00 +0100

    install script

--- install.sh
+++ install.sh
@@ -1,3 +1,30 @@
 #!/bin/sh
 
-# Doesn't do anything yet!!
+# Archive existing dot files and symlink to the new ones
+# Run from ~/dotfiles
+
+# All dot files apart from .git
+dotFiles=(`ls -d .* | grep -v '.git'`)
+
+# Existing dot files to archive
+toArchive=()
+
+cd ~
+
+# Archive
+for i in ${dotFiles[*]}; do
+	toArchive=(${toArchive[@]} `ls -d $i`)
+done
+if [ ${#toArchive[*]} != 0 ]; then
+	archiveFile="dotfiles"`date +%Y%m%d`".tar"
+	tar -cf $archiveFile $toArchive | gzip -9 $archiveFile
+
+	# Delete clashes
+	rm -rf $toArchive
+fi
+
+# Create symlinks
+for i in ${dotFiles[*]}; do
+	dotFile="dotfiles/"$i
+	ln -s $dotFile $i
+done
